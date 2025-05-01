440558026e8d49c61a45d7041e06a7d6a836157e
Dhurgham Faris <d6r9g69@gmail.com>
2025-05-01 23:36:21 +0300

    I AM BATMAN

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SiteTitle = "Salis Release Notes Archive";
+export const SiteDescription =
+  "This is the archive of all Salis release notes.";
